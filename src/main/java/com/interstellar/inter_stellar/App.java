d37f0a5680930313806f48f714ad4361c652d0e8
Varun <chakrigupta473@gmail.com>
2022-02-19 11:34:52 -0500

    added first commit

---
+++ src/main/java/com/interstellar/inter_stellar/App.java
@@ -0,0 +1,13 @@
+package com.interstellar.inter_stellar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
